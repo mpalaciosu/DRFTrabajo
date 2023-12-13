c088e088f4aeb4a1a46afc6b4eee6519373cb89a
Martin Palacios Undurraga <martinpalaciosu@gmail.com>
2023-12-13 19:24:02 -0300

    Cambios varios

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='0.0.1',
     description='Repositorio para el trabajo de DRF',
     url='https://github.com/mpalaciosu/DRFTrabajo',
-    author='Miguel Palacios',
-    author_email='mpalaciosu@gmail.com',
+    author='Martin Palacios',
+    author_email='martinpalaciosu@gmail.com',
     packages=find_packages(),
     python_requires='>=3.8',
 )
