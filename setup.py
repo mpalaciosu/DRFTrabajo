e6e9fb5188dac5f9c8b97e971f507c9d3812be3b
Martin Palacios Undurraga <martinpalaciosu@gmail.com>
2023-12-13 19:08:30 -0300

    cambie el setup

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='DRFTrabajo',
-    version='0.1',
+    version='0.0.1',
+    description='Repositorio para el trabajo de DRF',
+    url='https://github.com/mpalaciosu/DRFTrabajo',
+    author='Miguel Palacios',
+    author_email='mpalaciosu@gmail.com',
     packages=find_packages(),
-    install_requires=[
-        # List any dependencies your project has
-    ],
+    python_requires='>=3.8',
 )
