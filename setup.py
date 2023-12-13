688dd1e493037e35d0310e07a647622540b6cf46
Martin Palacios Undurraga <111102519+mpalaciosu@users.noreply.github.com>
2023-12-13 17:21:14 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 13 00:42:34 2023
-
-@author: marti
-"""
-
 from setuptools import setup
 
 setup(
-    name="uf_calculator",
-    version="0.1.0",  
-    description="Calculate the UF value for any day in Chile",
-    author="Martin Palacios", 
-    author_email="martinpalaciosu@gmail.com",  
-    url="https://github.com/mpalaciosu/DRFTrabajo.git",  
-    packages=["uf_calculator"],  
-    install_requires=[
-        "datetime",
-        "calendar",
-    ],
-    python_requires=">=3.6",  
+    name="DRFTrabajo",
+    version="0.1",
+    description="Proyecto DRFTrabajo",
+    packages=["DRFTrabajo"],
+    python_requires=">=3.7",
 )
