467fb6f63d6f23c83f4f47846802b81f03c5afcc
Martin Palacios Undurraga <martinpalaciosu@gmail.com>
2023-12-19 15:31:32 -0300

    Reparacion funcion

--- DRFTrabajo/entregable3.py
+++ DRFTrabajo/entregable3.py
@@ -1,29 +1,30 @@
 import datetime as dt 
 from datetime import timedelta, datetime
 import calendar
+from datetime import date
 
-def get_ufs(last_uf_known_date : dt.datetime, last_uf_value : float, new_ipc : float):
+def get_ufs(last_uf_known_date : date, last_uf_value : float, new_ipc : float) -> dict[date, float]:
     """
     Funcion calcula el valor de la UF para hoy en base a inputs.
 
     Parameters
     ----------
-    last_uf_known_date : dt.datetime
-        Fecha de la ultima uf conocida para un dia 9. (este dato siempre estara
-        disponible online).
+    last_uf_known_date : date
+        Fecha de la ultima uf conocida (previo al dia el cual se quiere calcular).
     last_uf_value : float
-        Valor de la UF el ultimo dia 9.
+        Valor de la UF del ultimo dia conocido.
     new_ipc : float
-        IPC publicado el mes anterior. Se debe utilizar el valor mensual.
+        ultimo ipc mensual publicado en formato de numero, es decir 0.7% se debe
+        ingresar como 0.007 en le input.
 
     Returns
     -------
-    [fecha_hoy, uf_hoy] : DICT
+    [fecha_hoy, uf_hoy] : dict
         Diccionario con la informacion relevante para hoy.
 
     """
     
-    fecha_hoy = datetime.now()
+    fecha_hoy = last_uf_known_date + timedelta(days=1)
     i = (fecha_hoy - last_uf_known_date).days
     
     if fecha_hoy.day > 9: 
@@ -39,18 +40,16 @@ def get_ufs(last_uf_known_date : dt.datetime, last_uf_value : float, new_ipc : f
         
     else:
         
-        first_day_previous_month = last_uf_known_date.replace(day=1) - timedelta(days=1)
+        last_day_previous_month = last_uf_known_date.replace(day=1) - timedelta(days=1)
         
-        last_day_previous_month = first_day_previous_month + timedelta(
-            days=calendar.monthrange(
-                first_day_previous_month.year, 
-                first_day_month.month)[1] - 1)
+        first_day_previous_month = last_day_previous_month.replace(day=1)
         
         d = (last_day_previous_month - first_day_previous_month).days + 1
 
-    aux = last_uf_value*(1 + new_ipc) ** (i/d)
+    aux = last_uf_value * (1 + new_ipc) ** (i/d)
     
     uf_hoy = round(aux, 2)
 
-    return [fecha_hoy, uf_hoy]
+    return {'date':fecha_hoy,'value': uf_hoy}
+
 
